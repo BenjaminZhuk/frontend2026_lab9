69671b90733ff3e537a60fdb2b96ba41008edf27
Benjamin Zhuk bc2025-2 <veniamin.zhuk@lnu.edu.ua>
2026-05-29 14:23:54 +0300

    Додав базу даних потягів та маршрутів для моделювання

---
+++ src/data/trains.js
@@ -0,0 +1,42 @@
+export const mockTrains = [
+  {
+    id: "705K",
+    trainNumber: "705К",
+    from: "Київ",
+    to: "Перемишль",
+    departureDate: "2026-06-01",
+    departureTime: "06:00",
+    duration: "7 г. 20 хв.",
+    price: 1200
+  },
+  {
+    id: "012K",
+    trainNumber: "012К",
+    from: "Київ",
+    to: "Ужгород",
+    departureDate: "2026-06-01",
+    departureTime: "21:15",
+    duration: "12 г. 45 хв.",
+    price: 750
+  },
+  {
+    id: "091Л",
+    trainNumber: "091Л",
+    from: "Львів",
+    to: "Київ",
+    departureDate: "2026-06-02",
+    departureTime: "22:40",
+    duration: "8 г. 10 хв.",
+    price: 680
+  },
+  {
+    id: "007К",
+    trainNumber: "007К",
+    from: "Харків",
+    to: "Одеса",
+    departureDate: "2026-06-02",
+    departureTime: "16:13",
+    duration: "11 г. 30 хв.",
+    price: 890
+  }
+];
